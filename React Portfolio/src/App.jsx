4bb2c7c97d655aae606608357b56055bb433538b
Adam Olson <adam.lee.olson@outlook.com>
2023-11-06 23:28:31 -0600

    layout of contact me section

--- React Portfolio/src/App.jsx
+++ React Portfolio/src/App.jsx
@@ -5,14 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'; //needs to go here above appl
 import './App.css'
 
 function App() {
-  
-
   return (
-    <>
-    <PortfolioContainer />;
-    
-    </>
-  )
+    <div style={{ backgroundColor: '#156064' }}> {/* Set the background color for the entire page */}
+      <PortfolioContainer />
+    </div>
+  );
 }
 
 export default App
--- React Portfolio/src/components/pages/Contact.jsx
+++ React Portfolio/src/components/pages/Contact.jsx
@@ -1,13 +1,42 @@
-function Contact() {
+import React from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
-    return (
-    <h1 className="section-title"> Contact Me </h1>
-    
-    )
-    }
-    
-    
-    //creating a base template here.  
-    
-    
-    export default Contact;
+function Contact() {
+  return (
+    <Container>
+      <h2>Contact Me</h2>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>First Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter your first name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter your last name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="Enter your email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control type="tel" placeholder="Enter your phone number" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={4} placeholder="Enter your message" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default Contact;
